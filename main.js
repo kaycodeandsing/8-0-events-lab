3d9170fef15900f4d046b8febd00ac9939f72c7a
Karimah Reavis <Karimah586@gmail.com>
2022-12-30 00:39:07 -0500

    finished lab

--- main.js
+++ main.js
@@ -7,3 +7,51 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+let activePenTool= false
+const currentColor = document.getElementById("current-color");
+const palleteColor = document.querySelectorAll(".color");
+palleteColor.forEach(color => {
+  color.addEventListener("click", (event) => {
+ currentColor.style.background = event.target.style.backgroundColor; 
+  });
+});
+
+const cellsInMain = document.querySelectorAll(".cell");
+
+cellsInMain.forEach((cell) => {
+  cell.addEventListener("mouseover", draw); 
+  cell.addEventListener("dblclick", erase);
+  cell.addEventListener("click", togglePen);
+});
+
+function erase(event) {
+  event.target.style.background = "white";
+}
+
+function draw(){
+  if(activePenTool){
+    this.style.backgroundColor = currentColor.style.backgroundColor;
+  }
+}
+
+function togglePen(event){
+  if(!activePenTool){
+    activePenTool = true;
+  } else {
+    activePenTool = false;
+  }
+}
+
+const eraseEverything = document.createElement("div");
+eraseEverything.classList.add("color");
+eraseEverything.style.cssText = "border-radius: 10px; position: absolute; background: silver; ";
+eraseEverything.innerText = "Erase";
+
+const palette = document.querySelector("#palette");
+palette.append(eraseEverything);
+
+eraseEverything.addEventListener("click", () => {
+  cellsInMain.forEach((cell) => {
+    cell.style.background = "white";
+  });
+});
